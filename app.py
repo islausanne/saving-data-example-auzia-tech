ace454309c463d270adda52d36dc19bc39455a93
auzia-tech <auzia@islstudent.ch>
2025-11-13 14:49:54 +0100

    Date:13/11/2025 Lesson number: 2 1. Goal for today (C i)  State what your plan said you should complete during this session. My original plan was to start working on filling out the website with images and I started work on it. 2. Work completed (C iii)  Write one or two sentences summarising what you achieved in this session. In this session I added two images into the website but Its still not working and thats my goal for before next class to finish it up.
    3. Did you follow your plan? (C iii and C iv)
    It followed my plan a little bit but I wanted to finish it.
    
    (When you complete this section, you are providing evidence for Criterion C iv.)
    4. Next step (C i)
     I want to make it so the contact information area works and to fix the submit part

--- app.py
+++ app.py
@@ -3,36 +3,49 @@ import json
 import os
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # Needed for flash messages
+app.secret_key = 'supersecretkey'
 
-# Home route
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Registration form page
+
 @app.route('/register')
 def register():
     return render_template('register.html')
 
-# Handle form submission (students will add JSON save code here)
+
 @app.route('/submit', methods=['POST'])
 def submit_form():
     name = request.form['name']
     country = request.form['country']
     age = request.form['age']
 
+
+    data.append({'name': name, 'country': country, 'age': age})
+
+
+    with open('registrations.json', 'w') as file:
+        json.dump(data, file, indent=2)
+
     # TODO: Save form data to a JSON file (worksheet Part 1)
 
     flash('Registration submitted successfully!')
     return redirect(url_for('index'))
 
-# Display stored registrations (students will add JSON reading code here)
+
 @app.route('/view')
 def view_registrations():
     # TODO: Read data from registrations.json and send to template (worksheet Part 2)
-
-    return render_template('view.html', registrations=[])
+    @app.route('/view')
+    def view_registrations():
+        with open('registrations.json', 'r') as file:
+            data = json.load(file)
+        return render_template('view.html', registrations=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
